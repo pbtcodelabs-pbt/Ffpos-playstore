a10dc5da7d5060d658fc32faec6ad0f0ddeebc0d
pbtcodelabs-pbt <pbtcodelabs@gmail.com>
2026-08-12 11:51:40 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // FF POS — Service Worker (بنیادی آف لائن سپورٹ)
-const CACHE_NAME = 'ffpos-cache-12aug1133am';
+const CACHE_NAME = 'ffpos-cache-12aug1149am';
 const CORE_ASSETS = [
   './',
   './index.html',
